de87fc885b87bdfb50e8c08fa4a9bc59cfa1a969
sad8787 <43324098+sad8787@users.noreply.github.com>
2021-11-19 17:24:36 +0300

    19 nov 2021
    ee

---
+++ 10and10/Controllers/ElementosController1.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using _10and10.Models;
+
+namespace _10and10.Controllers
+{
+    public class ElementosController1 : Controller
+    {
+
+        private List<Elemento> listelementos = new List<Elemento>();
+        public ElementosController1()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                listelementos.Add(new Elemento(i));
+            }
+        }        
+             
+        public IActionResult Index(int? id)
+        {
+            int a = 0;
+            if (id == null)
+                a= 0;
+            else
+                a = int.Parse(id.ToString());
+            if (a == -1|| a >= listelementos.Count)//final
+                a = listelementos.Count-10;
+            if (a < 0)
+                a = 0;            
+
+            List<Elemento> elem = new();
+            for (int i = a; (i < a + 10) && (i < listelementos.Count); i++)
+            {
+                elem.Add(listelementos[i]);
+            }
+            ViewBag.NumTimes = a + 10;
+            return View(elem);
+        }       
+        
+        
+        
+    }
+}
---
+++ 10and10/Models/Elemento.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _10and10.Models
+{
+    public class Elemento
+    {
+        public Elemento(int id)
+        {
+            
+            Name = "Elemento Name "+(id+1);
+            Id = id;
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
