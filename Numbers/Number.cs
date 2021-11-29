72ad40eea0688e130c7ccd49c2d00a1eb8eb8041
sad8787 <43324098+sad8787@users.noreply.github.com>
2021-11-29 16:12:52 +0300

    number
    numeros

---
+++ Numbers/Number.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberBeautiful
+{
+    public class Number
+    {
+        int[] valueA = new int[6];
+        public Number()
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                valueA[i] = 0;
+            }
+        }
+        public int[] GetSetvalueA
+        {
+            get { return valueA; }
+            set { valueA = value; }
+        }
+        public int Sum()
+        {
+            return valueA[0] + valueA[1] + valueA[2] + valueA[3] + valueA[4] + valueA[5];
+        }
+        public bool Cero()
+        {
+            return (valueA[5] == 0);
+        }
+        public bool Last()
+        {
+            return (valueA[0] == 12 && valueA[1] == 12 && valueA[2] == 12 && valueA[3] == 12 && valueA[4] == 12 && valueA[5] == 12);
+        }
+        public void Increase(int poss) //desde 6 a 0
+        {
+            if (poss >= 0)
+            {
+                Boolean a = auxIncrease(poss);
+                if (a == true)
+                {
+                    Increase(poss - 1);
+                }
+            }
+        }
+        public bool auxIncrease(int a)
+        {
+            if (valueA[a] == 12)
+            {
+                valueA[a] = 0;
+                return true;
+            }
+            else
+            {
+                valueA[a] += 1;
+                return false;
+            }
+        }
+    }
+}
+    
---
+++ Numbers/Program.cs
@@ -0,0 +1,46 @@
+﻿using NumberBeautiful;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Ther!");
+            ulong cont = 0;
+            ulong[] a = new ulong[61];
+            ulong[] b = new ulong[61];
+            Number numberA = new Number();
+            //Thread thread0=null;
+            while (!numberA.Last())//
+            {                
+                int s = numberA.Sum();                
+                if ( s<= 60)
+                {
+                    a[s]++;
+                    if (numberA.Cero())
+                    {
+                        b[s]++;
+                       // Console.WriteLine(numberA.Cadena() + " " + s);
+                    }
+                }
+                numberA.Increase(5);           
+            }
+            for (int s = 0; s < 61; s++)
+            {
+                if (a[s]!=0 && b[s]!=0)
+                {
+                    cont += a[s] * b[s] * 13;
+                }
+            }
+            Console.WriteLine("");
+            Console.WriteLine("The total of beautiful numbers with a leading zero is: " + cont);
+            Console.ReadKey();
+
+        }        
+       
+    }
+}
