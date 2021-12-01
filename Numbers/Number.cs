a6f305a169d4acfc8f70c1e0de9818ea1730e82e
sad8787 <43324098+sad8787@users.noreply.github.com>
2021-12-01 14:03:09 +0300

    e
    e

--- Numbers/Number.cs
+++ Numbers/Number.cs
@@ -6,12 +6,13 @@ using System.Threading.Tasks;
 
 namespace NumberBeautiful
 {
+    //00xxxxXxxxx00
     public class Number
     {
-        int[] valueA = new int[6];
+        int[] valueA = new int[4];
         public Number()
         {
-            for (int i = 0; i < 6; i++)
+            for (int i = 0; i < 4; i++)
             {
                 valueA[i] = 0;
             }
@@ -19,17 +20,18 @@ namespace NumberBeautiful
         
         public int Sum()
         {
-            return valueA[0] + valueA[1] + valueA[2] + valueA[3] + valueA[4] + valueA[5];
+            //return valueA[0] + valueA[1] + valueA[2] + valueA[3] + valueA[4] + valueA[5];
+            return valueA[0] + valueA[1] + valueA[2] + valueA[3] ;
         }
         public bool FirstCero()
         {
             return (valueA[0] == 0 && valueA[1] == 0);
         }
-        public bool Last()//CCCC01
+        public bool Last()//CCCC
         {
-            return (valueA[0] == 12 && valueA[1] == 12 && valueA[2] == 12 && valueA[3] == 12 && valueA[5] == 1);//CCCC01
+            return (valueA[0] == 12 && valueA[1] == 12 && valueA[2] == 12 && valueA[3] == 12 );//CCCC
         }
-        public void Increase(int poss) //desde 6 a 0
+        public void Increase(int poss) //desde 3 a 0
         {
             if (poss >= 0)
             {
--- Numbers/Program.cs
+++ Numbers/Program.cs
@@ -7,30 +7,33 @@ namespace ConsoleApp1
 {
     class Program
     {
+        //00xxxxXxxxx00
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Ther!");
             long cont = 0;
             long[] a = new long[49];
-            long[] b = new long[49];
+            //long[] b = new long[49];
             Number numberA = new Number();            
             while (!numberA.Last())//
             {                
                 int s = numberA.Sum();                
                 if ( s<= 48)//max Sum =0+0+12+12+12+12=48
                 {
-                    b[s]++;
-                    if (numberA.FirstCero())
+                    //b[s]++;
+                    /*if (numberA.FirstCero())
                     {
                         a[s]++;
-                    }
-                   
+                    }*/
+                    a[s]++;
                 }
-                numberA.Increase(5);           
+                numberA.Increase(3);           
             }
+            a[48]++;
             for (int s = 0; s < 49; s++)
-            {      
-                    cont += a[s] * b[s] * 13;              
+            {
+                // cont += a[s] * b[s] * 13;
+                 cont += a[s] * a[s] * 13;  
             }
             Console.WriteLine("");
             Console.WriteLine("The total of beautiful numbers with leading zeros is: " + cont);
