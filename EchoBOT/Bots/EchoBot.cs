eecc1debdaa9ba2ce8d2bde75b04800452505fd9
sad8787 <43324098+sad8787@users.noreply.github.com>
2022-05-13 14:00:19 +0300

    robot
    robot

---
+++ EchoBOT/Bots/EchoBot.cs
@@ -0,0 +1,61 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+//
+// Generated with Bot Builder V4 SDK Template for Visual Studio EchoBot v4.15.2
+
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using AdaptiveCards;
+
+namespace EchoBOT.Bots
+{
+    public class EchoBot : ActivityHandler
+    {
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var replyText = $"Echo: {turnContext.Activity.Text}";
+            if (replyText.Contains("время")|| replyText.Contains("Время"))
+            {
+                try
+                {
+                    var card = new HeroCard
+                    {
+                        Title = "Время?!",
+                        Text = @"Сегодня " + DateTime.Now.ToString("dd,MMMM,yyyy HH:mm tt"),
+                        Images = new List<CardImage>() { new CardImage("http://adaptivecards.io/content/cats/1.png") }
+                    };
+                    var response = MessageFactory.Attachment(card.ToAttachment());
+                    await turnContext.SendActivityAsync(response, cancellationToken);
+                   
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            else
+            {
+                replyText = "напишите, пожалуйста";
+                await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
+            }
+            //await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
+        }
+
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var welcomeText = "Привет и добро пожаловать. Я робот ЭХО. Создано Садиэлем!";
+            foreach (var member in membersAdded)
+            {
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    await turnContext.SendActivityAsync(MessageFactory.Text(welcomeText, welcomeText), cancellationToken);
+                }
+            }
+        }
+    }
+}
