354a8f659405457663a7d77d77ff6d06f7cebca6
k.godales <k.godales@chelpipegroup.com>
2024-02-13 13:38:04 +0300

    Duplas
    Для отклика на эту вакансию работодатель предлагает ответить на вопрос.
    
    Пожалуйста, решите следующую простую задачу. Можно использовать любые инструменты.
    Дано: список dict-объектов вида вида {"key": "value"}, например [{"key1": "value1"}, {"k1": "v1", "k2": "v2", "k3": "v3"}, {}, {}, {"key1": "value1"}, {"key1": "value1"}, {"key2": "value2"}].
    
    Напишите функцию, которая удаляет дубликаты из этого списка. Для примера выше возвращаемое значение может быть равно [{"key1": "value1"}, {"k1": "v1", "k2": "v2", "k3": "v3"}, {}, {"key2": "value2"}].
    Обязательное условие: функция не должна иметь сложность O(n^2).

---
+++ Console_List_Delete_repited/Dupla.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_List_Delete_repited
+{
+    public class Dupla
+    {
+        public string key { get; set; }
+        public string value { get; set; }
+        public Dupla(string key, string value) {
+            this.key = key;
+            this.value = value;
+        }
+        public override bool Equals(object obj)
+        {
+            return (((Dupla)obj).key + ((Dupla)obj).value) == key + value;
+        }
+        public override int GetHashCode()
+        {
+            return (key + value).GetHashCode();
+        }
+    }
+}
---
+++ Console_List_Delete_repited/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Console_List_Delete_repited
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                       
+            Console.WriteLine("Duplas");
+            List<Dupla> list_duplas = new List<Dupla>();
+            list_duplas.Add(new Dupla("Key1", "Value1"));
+            list_duplas.Add(new Dupla("Key1", "Value2"));
+            list_duplas.Add(new Dupla("Key1", "Value1"));
+            list_duplas.Add(new Dupla("Key2", "Value1"));
+            list_duplas.Add(new Dupla("Key2", "Value2"));
+            list_duplas.Add(new Dupla("Key3", "Value1"));
+            list_duplas.Add(new Dupla("Key3", "Value2"));
+            list_duplas.Add(new Dupla("Key3", "Value3"));
+            list_duplas.Add(new Dupla("Key3", "Value1"));
+            list_duplas.Add(new Dupla("Key3", "Value2"));
+            List<Dupla> distinct_Dupla_List = list_duplas.Distinct().ToList();
+
+            foreach (var ele in distinct_Dupla_List)
+            {
+                Console.WriteLine("Key = {0}, Value = {1}", ele.key, ele.value);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
